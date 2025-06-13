3761775093e1fa7fb5c7f82c96cb2002ced33587
Ibroximjon <makhammadjonovibrokhimjon@gmail.com>
2025-06-13 09:13:54 +0500

    adds logic of listener

--- training-summary-service/src/main/java/com/ibroximjon/trainingsummaryservice/listener/TrainingSummaryListener.java
+++ training-summary-service/src/main/java/com/ibroximjon/trainingsummaryservice/listener/TrainingSummaryListener.java
@@ -4,12 +4,37 @@ package com.ibroximjon.trainingsummaryservice.listener;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class TrainingSummaryListener {
+
+    private final Map<String, Map<YearMonth, Integer>> summaryMap = new HashMap<>();
+
 
     @JmsListener(destination = "training-summary-queue")
     public void receive(TrainingEventRequest event) {
         System.out.println("Received training event: " + event.getTrainerUsername());
-        // davomiylikni hisoblash logikasi shu yerda bo‘ladi
+
+        String username = event.getTrainerUsername();
+        LocalDate date = event.getTrainingDate();
+        YearMonth yearMonth = YearMonth.from(date);
+        int hours = event.getTrainingDuration();
+
+        Map<YearMonth,Integer> trainerStats = summaryMap.getOrDefault(username, new HashMap<>());
+
+        int updatedHours = trainerStats.getOrDefault(yearMonth, 0)+hours;
+        trainerStats.put(yearMonth, updatedHours);
+        summaryMap.put(username, trainerStats);
+        System.out.println("Updated trainer stats: " + trainerStats);
+    }
+
+
+    //for testing
+    public Map<String, Map<YearMonth, Integer>> getSummaryMap() {
+        return summaryMap;
     }
 }
